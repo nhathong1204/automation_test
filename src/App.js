2055b6e35918279051144007bf6cee1095e98ea5
hanguyen <hanguyen@ip-192-168-1-7.ap-southeast-1.compute.internal>
2024-06-17 22:20:28 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { useState} from 'react'
 
 function App123456789(a) {
   //sua code tai day
+  //aaaaa
   // se bi conflict code tai day
   const [job, setJob] = useState('')
   const [jobs, setJobs] = useState(() => {
